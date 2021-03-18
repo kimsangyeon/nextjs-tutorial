640abfecb2851e7678fa8acf5468d6f85bed8211
kakao <kakao@kakaoui-MacBookPro.local>
2021-03-18 16:50:41 +0900

    lesson/4

---
+++ pages/404.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Icon } from 'semantic-ui-react';
+
+const Error404 = () => (
+  <div style={{ padding: '200px 0', textAlign: 'center', fontSize: 30 }}>
+    <Icon name="warning circle" color="red" />
+    404 : 페이지를 찾을 수 없습니다.
+  </div>
+);
+
+export default Error404;
